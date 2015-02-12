86b4d6a3b67b03d5b01517bcb4a041cfc365f207
Lubimov Igor <infunt@gmail.com>
2015-02-12 17:48:54 +0200

    Removed unused code

--- verification/src/main.py
+++ verification/src/main.py
@@ -1,20 +1,10 @@
-import argparse
 import coloredlogs
-import logging
 import signal
 import sys
 
 from tornado.ioloop import IOLoop
 
 from referee import Referee
-
-import sys
-# print(sys.argv)
-#
-# parser = argparse.ArgumentParser(description='Command line interface for CheckiO referee')
-# parser.add_argument('-h', '--data_server_host', help='Host of data server', required=True)
-# parser.add_argument('-p', '--data_server_port', help='Port of data server', required=True)
-# options = parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -24,14 +14,7 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, handle_signal)
     coloredlogs.install()
 
-    # if not options:
-    #     logging.error('No input options')
-    #     sys.exit(0)
-    #
     io_loop = IOLoop.instance()
-    #
-    # host = options.data_server_host
-    # port = options.data_server_host
 
     host = sys.argv[1]
     port = int(sys.argv[2])
