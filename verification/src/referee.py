8919ab11fb7a8834a089487bdc70e523a5e48ad0
Lubimov Igor <infunt@gmail.com>
2015-02-24 11:43:22 +0200

    added settings env

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,9 +1,11 @@
 from checkio_referee import RefereeBase
 
 import settings
+import settings_env
 from tests import TESTS
 
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
+    CURRENT_ENV = settings_env.CURRENT_ENV
