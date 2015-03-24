f039c76927dc1262ba23607e28cb7e3e78defbb8
Lubimov Igor <infunt@gmail.com>
2015-03-24 12:42:54 +0200

    added platform condition

--- verification/envs/python_3/run.sh
+++ verification/envs/python_3/run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-SCRIPT=`realpath -s $0`
-SCRIPTPATH=`dirname $SCRIPT`
 
-# TODO: find right way, that will work in docker/linux/mac os
-#SCRIPTPATH=$( cd "$( dirname "$BASH_SOURCE" )" && pwd )  # for macos
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    SCRIPT=`realpath -s $0`
+    SCRIPTPATH=`dirname $SCRIPT`
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    SCRIPTPATH=$( cd "$( dirname "$BASH_SOURCE" )" && pwd )
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#elif [[ "$OSTYPE" == "win32" ]]; then
+#elif [[ "$OSTYPE" == "freebsd"* ]]; then
+else
+    SCRIPT=`realpath -s $0`
+    SCRIPTPATH=`dirname $SCRIPT`
+fi
 
 python3 $SCRIPTPATH/main.py $1 $2
