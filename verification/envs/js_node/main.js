fffae0f717f8e5d23795208369d0e2362c816cee
Oleksandr Liabakh <oduvan@Oleksandrs-MacBook-Pro.local>
2015-06-09 19:17:08 +0300

    jslint verification

--- verification/envs/js_node/main.js
+++ verification/envs/js_node/main.js
@@ -1,5 +1,5 @@
 var optimist = require('optimist');
-var ClientLoop = require('./client.js').ClientLoop
+var ClientLoop = require('./client.js').ClientLoop;
 
 var argv = optimist.argv._;
-client = new ClientLoop(argv[0], argv[1])
+var client = new ClientLoop(argv[0], argv[1]);
